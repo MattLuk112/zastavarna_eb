24ad8d6a41af95205bacf3168463510c02de215b
matejpozdravuje <matej@bitterend.io>
2020-12-11 16:09:58 +0100

    Disable edit on contract

--- plugins/contract/admin/components/ContractEditActions.vue
+++ plugins/contract/admin/components/ContractEditActions.vue
@@ -6,11 +6,11 @@
   >
     Smazat
   </button>
-  <button
+  <!-- <button
     type="button"
     class="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-green-100 bg-green-600 border border-green-400 rounded-md shadow-xl group hover:text-white focus:outline-none focus:text-white focus:border-green-500 hover:border-green-500"
     @click="$emit('save')"
   >
     Uložit
-  </button>
+  </button> -->
 </template>
